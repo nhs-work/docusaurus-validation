31a9e2b4f12270dfa8a0b6fe2264198c7267a7d8
nhs-work <nhs.work.95@gmail.com>
2023-08-18 17:41:55 +0800

    feat(*): test jwks endpoint

--- src/theme/Root.js
+++ src/theme/Root.js
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from "axios";
 
 // Default implementation, that you can customize
 export default function Root({children}) {
+  useEffect(() => {
+    void (async () => {
+      const { data } = await axios.get("https://uat.api.dwp.gov.sg/extendedauth/api/jwks");
+      console.log(data);
+    })();
+  }, []);
   return <>{children}</>;
 }
